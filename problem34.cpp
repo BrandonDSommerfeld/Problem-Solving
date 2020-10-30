dafbcd2ae9e3eba3b78e5949deb7fcf075734bb1
BrandonDSommerfeld <bdsommerfeld@gmail.com>
2020-10-30 17:02:17 -0500

    Created files

---
+++ problem34.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include "math_unsigned.cpp"
+
+int main ()
+{
+    //Find the sum of all numbers which are equal to
+    //The sum of the factorial of their digits, 1 and 2 excluded
+    return 0;
+}
---
+++ problem35.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include "math_unsigned.cpp"
+
+int main ()
+{
+    //A circular prime is a number where all rotations of the digits are 
+    //prime: 197, 719, 971
+    //Find the number of such primes below 1000000
+    return 0;
+}
