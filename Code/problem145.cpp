7be79fce853d985d979ba61d886390589e57c2cf
Brandon Sommerfeld <bdsommerfeld@gmail.com>
2021-03-16 11:53:02 -0400

    Have an idea, moving to laptop for later

---
+++ Code/problem145.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <set>
+#include <queue>
+#include "math_unsigned.cpp"
+#include "math_signed.cpp"
+#include "algorithms.cpp"
+
+int main ()
+{
+    //A number is reversible if adding it to its 
+    //reverse gives a number with only odd digits
+    //Leading 0's are not allowed on the number 
+    //or its reverse
+    //Find number of reversible numbers below 1 billion
+    
+    //First digit plus last must be odd for the units 
+    //digit to be odd
+    //Also means second digit plus second to last cannot overflow, 
+    //or else leading digit won't be odd
+
+    
+    return 0; 
+}
