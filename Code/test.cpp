98013c3ce283092b71ab8cdcb30e1ad6a9e31043
BrandonDSommerfeld <bdsommerfeld@gmail.com>
2024-04-16 14:27:15 -0400

    Used for homework

---
+++ Code/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <queue>
+#include <list>
+#include <map>
+#include <utility>
+#include <set>
+#include "math_unsigned.h"
+#include "math_signed.h"
+#include "math_rational.h"
+#include "math_fast_rational.h"
+#include "algorithms.h"
+
+int main() {
+  math::Unsigned val = algorithms::choose(128,64);
+  std::cout << val << '\n';
+  return 0;  
+}
